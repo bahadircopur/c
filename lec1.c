7f0cfa9a9176c536c28b89db43c2d35496bca75e
Bahadır <129467455+bahadircopur@users.noreply.github.com>
2025-03-19 13:32:25 +0300

    derslere devam

---
+++ lec1.c
@@ -0,0 +1,25 @@
+//paralel kenar giriş bilgisini temsil eder 6-9.satırlar 
+#include <stdio.h>
+int main(void)
+{
+    int kenar1, kenar2, alan, cevre;
+    printf("Uzun kenarı giriniz: ");
+    scanf("%d", &kenar1);
+    printf("Kısa kenarı giriniz: ");
+    scanf("%d", &kenar2);
+    return 0;
+
+
+}
+
+
+/*int main() {
+    int a, b;
+    printf("Enter the first side of the parallelogram: ");
+    scanf("%d", &a);
+    printf("Enter the second side of the parallelogram: ");
+    scanf("%d", &b);
+    printf("The area of the parallelogram is %d\n", a * b);
+
+    return 0;
+}*/
